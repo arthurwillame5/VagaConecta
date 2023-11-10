582e1bbd75f1038011665a1f544e40266ad2c2fa
arthurwillame5 <135058422+arthurwillame5@users.noreply.github.com>
2023-11-10 11:46:51 -0300

    Foi adicionado um novo componente para o botão caixa

---
+++ src/components/ButtonCaixa/index.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, TouchableOpacity} from "react-native";
+import  Icon  from "react-native-vector-icons/AntDesign";
+
+
+const ButtonCaixa = ({}) => {
+    return (
+        <TouchableOpacity style = {styles.button}>
+            <Text style={styles.buttonText}>Informações do Pátio</Text>
+            <Icon style={styles.icon} name="profile" size={45}/>
+        </TouchableOpacity>
+        
+    );
+}
+
+
+
+export default ButtonCaixa;
+
+const styles = StyleSheet.create({
+    button: {
+       // backgroundColor: '#2BC9D3',
+        width: '100%',
+        height: '7%',
+        top: '17%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        //borderRadius: 16,
+        //borderWidth: 2,
+        borderColor: 'black',
+        marginTop: -18, 
+        fontSize: 15,       
+    },
+    buttonText: {
+        fontWeight: "bold",
+        top: '35%',
+        right: '2%'
+    },
+    icon: {
+        bottom: '17%',
+        right: '35%',
+    }
+}); 
