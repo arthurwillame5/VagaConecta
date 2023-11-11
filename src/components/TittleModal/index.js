dfb7821c4888e5a0b8ab5bd88d32c37b7abd3e0d
arthurwillame5 <135058422+arthurwillame5@users.noreply.github.com>
2023-11-11 11:31:23 -0300

    foi adicionado o componente tittleModal e exportado no modal

---
+++ src/components/TittleModal/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default function TitleModal() {
+  return (
+            <View>
+                <Text style={{textAlign: 'center', top: '70%', fontSize: 30, fontWeight: 'bold', right: '1%',}}>VagaConecta</Text>
+                <Text style={{textAlign: 'center', fontWeight: 'bold', fontSize: 20, top: '90%',}}>Estacionamento</Text>
+            </View>    
+  );
+}
+
